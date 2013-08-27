d2247238394484b79630116b7bb2d62edb648258
Ulf Gitschthaler <ulf.gitschthaler@comsysto.com>
2013-08-27 23:28:55 +0200

    Make database connection configurable

--- core/src/main/java/com/u6f6o/apps/moviedb/core/config/persistence/MongoDevConfig.java
+++ core/src/main/java/com/u6f6o/apps/moviedb/core/config/persistence/MongoDevConfig.java
@@ -1,26 +1,51 @@
 package com.u6f6o.apps.moviedb.core.config.persistence;
 
 import com.mongodb.Mongo;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.data.authentication.UserCredentials;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 @Configuration
 @Profile( "dev" )
-public class MongoDevConfig {
-    private static final String DB_NAME = "devMovieDB";
-    private static final String DB_HOST = "127.0.0.1";
+public class MongoDevConfig implements EnvironmentAware{
+
+    private Environment environment;
 
     @Bean
     public MongoDbFactory mongoDbFactory() throws Exception {
-        return new SimpleMongoDbFactory( new Mongo( DB_HOST ), DB_NAME );
+        return new SimpleMongoDbFactory(new Mongo(getDBHostName()), getDBName(),
+                new UserCredentials(getDBUserName(), getDBUserPassword()));
     }
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
         return new MongoTemplate( mongoDbFactory());
+    }
+
+    private String getDBHostName(){
+        return environment.getProperty("DB_HOST_NAME");
+    }
+
+    private String getDBName(){
+        return environment.getProperty("DB_NAME");
+    }
+
+    private String getDBUserName(){
+        return environment.getProperty("DB_USER_NAME");
+    }
+
+    private String getDBUserPassword(){
+        return environment.getProperty("DB_USER_PW");
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
